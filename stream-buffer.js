8479ed354fab08abd819d42d41b1b8a8a14d32d2
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2023-11-16 21:56:54 +0600

    understand stream-buffer

---
+++ stream-buffer.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const fs = require("fs");
+// creating a server using raw node.js
+
+const server = http.createServer();
+
+// server listener
+server.on("request", (req, res) => {
+  if (req.url === "/read-file" && req.method === "GET");
+  // streaming file reading
+  const readableStream = fs.createReadStream("./texts/read.txt");
+  readableStream.on("data", (buffer) => {
+    res.write(buffer);
+  });
+
+  readableStream.on("end", () => {
+    res.end("Hello from world!");
+  });
+});
+
+server.listen(5000, () => {
+  console.log(`server is listening on port 5000`);
+});
